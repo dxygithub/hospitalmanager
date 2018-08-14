16f9be663f6ff42423d1f9de04747c442ac65500
17696011037 <40793606+17696011037@users.noreply.github.com>
2018-08-14 11:32:04 +0800

    zhou

--- src/main/java/com/dyhc/hospitalmanager/dao/DeskTypeMapper.java
+++ src/main/java/com/dyhc/hospitalmanager/dao/DeskTypeMapper.java
@@ -1,9 +1,19 @@
 package com.dyhc.hospitalmanager.dao;
 
+import com.dyhc.hospitalmanager.pojo.DeskType;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Mapper
 @Repository
 public interface DeskTypeMapper {
+
+    int addDesk_type(DeskType   deskType)throws Exception;//新增科室类型
+    DeskType   getById(@Param("id")Integer id);//根据id查询科室类型
+    int updDeskType (DeskType    deskType)throws Exception;//修改科室类型
+    List<DeskType> getAll()throws Exception; //查询科室类型
+    int delDeskType  (@Param("id")Integer id)throws Exception; //删除科室类型
 }
--- src/main/java/com/dyhc/hospitalmanager/dao/RoleTypeMapper.java
+++ src/main/java/com/dyhc/hospitalmanager/dao/RoleTypeMapper.java
@@ -2,6 +2,7 @@ package com.dyhc.hospitalmanager.dao;
 
 import com.dyhc.hospitalmanager.pojo.RoleType;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +11,41 @@ import java.util.List;
 @Repository
 public interface RoleTypeMapper {
 
-    List<RoleType> getRoleType();
+    /**
+     * 新增角色类型
+     * @param roleType 角色类型
+     * @return
+     * @throws Exception
+     */
+    int addRoleType(RoleType roleType)throws Exception;
+
+    /**
+     * 根据id查询角色类型
+     * @param id
+     * @return
+     */
+    RoleType  getById(@Param("id")Integer id)throws Exception;
+
+    /**
+     * 修改角色类型
+     * @param roleType
+     * @return
+     * @throws Exception
+     */
+    int updRoleType(RoleType roleType)throws Exception;
+
+    /**
+     * 查询所有角色类型
+     * @return
+     * @throws Exception
+     */
+    List<RoleType> getAllRoleTypeList()throws Exception;
+
+    /**
+     * 删除角色类型
+     * @param id
+     * @return
+     * @throws Exception
+     */
+    int delRoleTypeById(@Param("id")Integer id)throws Exception;
 }
