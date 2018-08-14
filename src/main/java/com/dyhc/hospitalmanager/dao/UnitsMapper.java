8e961d325dfb95830ba3bead040daecc541ff980
17696011037 <40793606+17696011037@users.noreply.github.com>
2018-08-14 08:44:45 +0800

    zhou

--- src/main/java/com/dyhc/hospitalmanager/dao/UnitsMapper.java
+++ src/main/java/com/dyhc/hospitalmanager/dao/UnitsMapper.java
@@ -2,25 +2,39 @@ package com.dyhc.hospitalmanager.dao;
 
 import com.dyhc.hospitalmanager.pojo.Units;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Mapper
 @Repository
 public interface UnitsMapper {
 
     /**
+     * 查询所有单位
+     * @return
+     */
+    List<Units> getAllUnitsList();
+
+    /**
      * 添加单位
      * @param units
      * @return
      */
-    int addUnits(Units units);
+    int addUnits(Units units) throws Exception;
 
     /**
      * 修改
      * @param units
      * @return
      */
-    int updUnits(Units units);
+    int updUnits(Units units) throws Exception;
 
-    
+    /**
+     * 根据单位编号，删除该单位
+     * @param unitsId 单位id
+     * @return
+     */
+    int delUnits(@Param("unitsId") String unitsId) throws Exception;
 }
