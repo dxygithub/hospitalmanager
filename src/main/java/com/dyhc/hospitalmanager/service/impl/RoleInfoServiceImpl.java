06d4cfb2c10e69e0379cfbb59efcecd4dbb1d5e0
wangweimin961228 <13546247909@163.com>
2018-08-15 14:42:40 +0800

    wwm

--- src/main/java/com/dyhc/hospitalmanager/service/impl/RoleInfoServiceImpl.java
+++ src/main/java/com/dyhc/hospitalmanager/service/impl/RoleInfoServiceImpl.java
@@ -1,6 +1,7 @@
 package com.dyhc.hospitalmanager.service.impl;
 
 import com.dyhc.hospitalmanager.dao.RoleInfoMapper;
+import com.dyhc.hospitalmanager.pojo.RoleInfo;
 import com.dyhc.hospitalmanager.service.RoleInfoService;
 import org.springframework.stereotype.Service;
 
@@ -11,4 +12,20 @@ public class RoleInfoServiceImpl implements RoleInfoService {
 
     @Resource(name = "roleInfoMapper")
     private RoleInfoMapper roleInfoMapper;
+
+    /**
+     * 根据用户名和密码进行登录判断
+     * @param userName
+     * @param pwd
+     * @return
+     */
+    @Override
+    public RoleInfo login(String userName, String pwd) {
+        RoleInfo roleInfo=roleInfoMapper.login(userName,pwd);
+        if (roleInfo!=null){
+            return roleInfo;
+        }else {
+            return null;
+        }
+    }
 }
