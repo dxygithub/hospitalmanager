ade74de29a37e6ab90cea2ac4dcc53343e5bf986
dxygithub <389228699@qq.com>
2018-08-15 15:14:34 +0800

    version1.3 update unitservice

--- src/main/java/com/dyhc/hospitalmanager/service/UnitsService.java
+++ src/main/java/com/dyhc/hospitalmanager/service/UnitsService.java
@@ -1,4 +1,39 @@
 package com.dyhc.hospitalmanager.service;
 
+import com.dyhc.hospitalmanager.pojo.Units;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ *单位信息维护业务
+ */
 public interface UnitsService {
+
+    /**
+     * 查询所有单位
+     * @return
+     */
+    List<Units> getAllUnitsList();
+
+    /**
+     * 添加单位
+     * @param units
+     * @return
+     */
+    int addUnits(Units units);
+
+    /**
+     * 修改
+     * @param units
+     * @return
+     */
+    int updUnits(Units units);
+
+    /**
+     * 根据单位编号，删除该单位
+     * @param unitsId 单位id
+     * @return
+     */
+    int delUnits(@Param("unitsId") String unitsId);
 }
