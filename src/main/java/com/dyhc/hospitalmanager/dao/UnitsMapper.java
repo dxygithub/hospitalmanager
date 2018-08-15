08e45867f7b48a64be794b0d7f530eb70f201375
dxygithub <389228699@qq.com>
2018-08-15 15:42:59 +0800

    version1.3 add units

--- src/main/java/com/dyhc/hospitalmanager/dao/UnitsMapper.java
+++ src/main/java/com/dyhc/hospitalmanager/dao/UnitsMapper.java
@@ -37,4 +37,17 @@ public interface UnitsMapper {
      * @return
      */
     int delUnits(@Param("unitsId") String unitsId) throws Exception;
+
+    /**
+     * 根据单位编号获取单位信息
+     * @param uid
+     * @return
+     */
+    Units getUnits(String uid) throws Exception;
+
+    /**
+     * 获取最后一个单位编号
+     * @return
+     */
+    String getUnitsNumber() throws Exception;
 }
