01b7c80198b14408d763e7f75a5551d47e1d060c
dxygithub <389228699@qq.com>
2018-08-16 17:41:41 +0800

    version1.3 add new html pages

--- src/main/java/com/dyhc/hospitalmanager/service/impl/PackageMedicalServiceImpl.java
+++ src/main/java/com/dyhc/hospitalmanager/service/impl/PackageMedicalServiceImpl.java
@@ -1,14 +1,34 @@
 package com.dyhc.hospitalmanager.service.impl;
 
 import com.dyhc.hospitalmanager.dao.PackageMedicalMapper;
+import com.dyhc.hospitalmanager.pojo.PackageMedical;
 import com.dyhc.hospitalmanager.service.PackageMedicalService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-
+import java.util.List;
+/**
+ * @author duxiaoyu
+ * 套餐关系业务实现
+ */
 @Service("packageMedicalServiceImpl")
 public class PackageMedicalServiceImpl implements PackageMedicalService {
 
     @Resource(name = "packageMedicalMapper")
     private PackageMedicalMapper packageMedicalMapper;
+
+    /**
+     * 显示套餐表下边的组合项的下边的体检项以及基本体检项
+     * @param packageId
+     * @return
+     */
+    public List<PackageMedical> getPackageInfoByPackageId(int packageId) {
+        List<PackageMedical> packageMedicals=null;
+        try {
+            packageMedicals=this.packageMedicalMapper.getPackageInfoByPackageId(packageId);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return packageMedicals;
+    }
 }
