61105d41187ec0942549b755622eb88bb125fe87
dxygithub <389228699@qq.com>
2018-08-16 18:33:59 +0800

    version1.3 add new html pages

--- src/main/java/com/dyhc/hospitalmanager/dao/GroupOrTestInfoMapper.java
+++ src/main/java/com/dyhc/hospitalmanager/dao/GroupOrTestInfoMapper.java
@@ -1,9 +1,17 @@
 package com.dyhc.hospitalmanager.dao;
 
+import com.dyhc.hospitalmanager.pojo.GroupOrTestInfo;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 @Mapper
 @Repository
 public interface GroupOrTestInfoMapper {
+
+    /**
+     * 保存单位分组和套餐关系
+     * @param groupOrTestInfo
+     * @return
+     */
+    Integer addGroupOrTestInfoMapper(GroupOrTestInfo groupOrTestInfo) throws Exception;
 }
