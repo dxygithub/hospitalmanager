129082f868f03291270310ffccfae119111fee43
dxygithub <389228699@qq.com>
2018-08-16 17:02:04 +0800

    version1.3 add new html pages

--- src/main/java/com/dyhc/hospitalmanager/service/MedicalService.java
+++ src/main/java/com/dyhc/hospitalmanager/service/MedicalService.java
@@ -1,4 +1,20 @@
 package com.dyhc.hospitalmanager.service;
 
+import com.dyhc.hospitalmanager.pojo.Medical;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * @author duxiaoyu
+ * 体检项业务
+ */
 public interface MedicalService {
+
+    /**
+     * 根据体检项编号查询该项目
+     * @param medicalId 项目项编号
+     * @return
+     */
+    Medical selMedicalById(@Param("medicalId") String medicalId);
 }
