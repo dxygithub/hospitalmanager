2269d750c1c7e84a8ea117115a57ef535a0b8840
wangweimin961228 <13546247909@163.com>
2018-08-16 12:47:45 +0800

    wwm

--- src/main/java/com/dyhc/hospitalmanager/controller/DeskController.java
+++ src/main/java/com/dyhc/hospitalmanager/controller/DeskController.java
@@ -1,7 +1,10 @@
 package com.dyhc.hospitalmanager.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.dyhc.hospitalmanager.service.DeskService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 
@@ -10,4 +13,11 @@ public class DeskController {
 
     @Resource(name = "deskServiceImpl")
     private DeskService deskService;
+
+    @RequestMapping("/getAllDesk.do")
+    @ResponseBody
+    public Object getDesk() throws Exception{
+        System.out.println(JSON.toJSONString(deskService.getDeskList()));
+        return JSON.toJSONString(deskService.getDeskList());
+    }
 }
