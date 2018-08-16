599f41360fd2e85416a4745a8cace8973dae01fc
dxygithub <389228699@qq.com>
2018-08-16 17:42:03 +0800

    version1.3 add new html pages

--- src/main/java/com/dyhc/hospitalmanager/service/PackageMedicalService.java
+++ src/main/java/com/dyhc/hospitalmanager/service/PackageMedicalService.java
@@ -1,4 +1,20 @@
 package com.dyhc.hospitalmanager.service;
 
+import com.dyhc.hospitalmanager.pojo.PackageMedical;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * @author duxiaoyu
+ * 套餐关系业务
+ */
 public interface PackageMedicalService {
+
+    /**
+     * 显示套餐表下边的组合项的下边的体检项以及基本体检项
+     * @param packageId
+     * @return
+     */
+    List<PackageMedical> getPackageInfoByPackageId(@Param("packageId") int packageId );
 }
