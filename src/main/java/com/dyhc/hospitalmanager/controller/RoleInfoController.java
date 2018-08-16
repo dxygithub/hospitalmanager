67c2989c5ff437f8164424b5edf3ca15f69cc7d5
17696011037 <40793606+17696011037@users.noreply.github.com>
2018-08-16 09:46:16 +0800

    zhou

--- src/main/java/com/dyhc/hospitalmanager/controller/RoleInfoController.java
+++ src/main/java/com/dyhc/hospitalmanager/controller/RoleInfoController.java
@@ -23,7 +23,8 @@ public class RoleInfoController {
     public Object login(RoleInfo roleInfo, HttpSession session){
         RoleInfo ro=roleInfoService.login(roleInfo.getUserName(),roleInfo.getPassword());
         if (ro!=null){
-            session.setAttribute(Commons.loginRoleInfo.toString(),ro);
+            String loginStr = Commons.loginRoleInfo.toString();
+            session.setAttribute(loginStr,ro);
             if (ro.getRoleTypeId()==roleInfo.getRoleTypeId()){
                 return JSON.toJSONString(ro.getRoleTypeId());
             }else {
@@ -32,5 +33,18 @@ public class RoleInfoController {
         }else {
             return JSON.toJSONString("您的用户名或密码错误！");
         }
+    }
+
+    /**
+     * 获取用户登录后的对象
+     * @return
+     */
+    @RequestMapping("getLoginRole.do")
+    @ResponseBody
+    public RoleInfo getLoginRole(HttpSession session){
+        String loing = Commons.loginRoleInfo.toString();
+        System.out.println(loing);
+        RoleInfo roleInfo = (RoleInfo)session.getAttribute(loing);
+        return roleInfo;
     }
 }
