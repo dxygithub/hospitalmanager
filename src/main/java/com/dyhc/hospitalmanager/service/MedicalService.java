a11081da0f4f92716bca43d52cfc5db2674ea1f7
17696011037 <40793606+17696011037@users.noreply.github.com>
2018-08-16 19:05:56 +0800

    zhou

--- src/main/java/com/dyhc/hospitalmanager/service/MedicalService.java
+++ src/main/java/com/dyhc/hospitalmanager/service/MedicalService.java
@@ -1,4 +1,34 @@
 package com.dyhc.hospitalmanager.service;
 
+import com.dyhc.hospitalmanager.pojo.conditional.MedicalConditional;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 public interface MedicalService {
+
+    /**
+     * 获取用户的体检项
+     * @param test_number 用户编号
+     * @return
+     * @throws Exception
+     */
+    List<MedicalConditional> getMadical(String test_number);
+
+    /**
+     * 获取用户的套餐项
+     * @param test_number 用户编号
+     * @return
+     * @throws Exception
+     */
+    List<MedicalConditional> getPackage(String test_number);
+
+    /**
+     * 获取用户组合项
+     * @param test_number 用户编号
+     * @return
+     * @throws Exception
+     */
+    List<MedicalConditional> getCom(String test_number);
+
 }
