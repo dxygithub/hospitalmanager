529a818aa17e46d71aba3455c563c0f23fc77d56
wangweimin961228 <13546247909@163.com>
2018-08-17 15:49:37 +0800

    wwm

--- src/main/java/com/dyhc/hospitalmanager/controller/DeskController.java
+++ src/main/java/com/dyhc/hospitalmanager/controller/DeskController.java
@@ -1,6 +1,7 @@
 package com.dyhc.hospitalmanager.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.dyhc.hospitalmanager.pojo.Desk;
 import com.dyhc.hospitalmanager.service.DeskService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +24,6 @@ public class DeskController {
     @RequestMapping("/getAllDesk.do")
     @ResponseBody
     public Object getDesk() throws Exception{
-        System.out.println(JSON.toJSONString(deskService.getDeskList()));
         return JSON.toJSONString(deskService.getDeskList());
     }
 
@@ -37,7 +37,33 @@ public class DeskController {
     @RequestMapping("/getDeskId.do")
     @ResponseBody
     public Object getIdDesk(@RequestParam("deskId")String deskId) throws Exception{
-        System.out.println(JSON.toJSONString(deskService.selDeksById(deskId)));
         return JSON.toJSONString(deskService.selDeksById(deskId));
+    }
+
+
+    /**
+     * 新增或修改
+     * @param desk
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping("/addORupd.do")
+    @ResponseBody
+    public Object addORupd(Desk desk) throws Exception{
+        System.out.println(desk.getDeskId());
+        Desk desk1=deskService.selDeksById(desk.getDeskId());
+        Integer num=0;
+        if (desk1!=null){
+            num=deskService.updDesk(desk);
+        }else {
+            num=deskService.addDesk(desk);
+        }
+        System.out.println(num);
+        if (num>0){
+            return "保存成功！";
+        }else {
+            return "保存失败！";
+        }
+
     }
 }
