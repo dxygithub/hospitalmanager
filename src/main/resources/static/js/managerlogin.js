3276bb68f4f82425c8daa254eb285ae3816e359f
crf-001 <927885007@qq.com>
2018-08-18 12:09:16 +0800

    crf

--- src/main/resources/static/js/managerlogin.js
+++ src/main/resources/static/js/managerlogin.js
@@ -1,9 +1,29 @@
+var $;
 //form表单提交事件
 layui.use('form', function() {
-	var form = layui.form;
-	//监听提交
-	form.on('submit(formDemo)', function(data) {
-		layer.msg(JSON.stringify(data.field));
-		return false;
-	});
+    var form = layui.form;
+    $=jQuery=layui.jquery;
+    //监听提交
+    form.on('submit(formDemo)', function(data) {
+        login();
+        return false;
+    });
 });
+
+function login() {
+    var roleInfo = $(".layui-form").serialize();
+    $.ajax({
+        url:"/loginAdmin.do",
+        data:roleInfo,
+        dataType:"json",
+        type:"post",
+        success:function (date) {
+            if (date=="登录成功") {
+                location.href="/Managerindex";
+            }else{
+                alert(date);
+            }
+        }
+
+    })
+}
